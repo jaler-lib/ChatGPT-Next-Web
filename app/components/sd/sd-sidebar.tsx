646c49f1423bf65cc65f3ebccdd92c6f12ba523d
jaler <463761833@qq.com>
2025-02-07 22:11:24 +0800

    Update sd-sidebar.tsx

--- app/components/sd/sd-sidebar.tsx
+++ app/components/sd/sd-sidebar.tsx
@@ -121,11 +121,6 @@ export function SideBar(props: { className?: string }) {
         <SdPanel />
       </SideBarBody>
       <SideBarTail
-        /**primaryAction={
-          <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
-            <IconButton icon={<GithubIcon />} shadow />
-          </a>
-        }**/
         secondaryAction={
           <IconButton
             text={Locale.SdPanel.Submit}
