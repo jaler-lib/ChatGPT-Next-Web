9099593545f3ef014a80d1395893f47c26376522
jaler <463761833@qq.com>
2025-02-07 20:56:10 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT
+          NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          每天进步一点点.
+          Build your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
